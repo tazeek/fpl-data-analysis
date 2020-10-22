3d25dba115d734f49213f1f398c151c7525f750e
tazeek <tazeek.rakib@gmail.com>
2020-10-22 14:19:31 +0800

    Filter function added

--- GameweekStats.py
+++ GameweekStats.py
@@ -7,3 +7,12 @@ class GameweekStats:
 
 		self.events_df = pd.DataFrame(json_file)
 		self.events_df = self.events_df[filtered_columns]
+
+
+	def filter(self):
+
+		# We only want data till the current gameweek
+		index_num = self.events_df[self.events_df['is_current']==True].index.values
+
+		# The slicing will be done one gameweek less; hence, the "+1" is needed
+		self.events_df = self.events_df.iloc[: index_num[0] + 1]
