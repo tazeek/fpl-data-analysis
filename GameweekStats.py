465a00422fc75e01525f743ab6ceda530d2a1ad1
tazeek <tazeek.rakib@gmail.com>
2020-10-23 14:28:42 +0800

    Function added

--- GameweekStats.py
+++ GameweekStats.py
@@ -24,3 +24,25 @@ class GameweekStats:
 
 		# Fetch the highest and average scores of the week
 		return self.events_df[['id','average_entry_score','highest_score']]
+
+	def fetch_chip_stats(self):
+
+		# Return all the chip statistics played every gameweek in a dataframe format
+		chip_stats_dict = {
+	    	'bboost': [],
+	    	'3xc': [],
+	    	'wildcard': [],
+	    	'freehit': []
+        }
+
+        chip_stats_dict = []
+
+		for gameweek in gameweek_stats.events_df['chip_plays']:
+    
+	    for chip_played in gameweek:
+	        
+	        print(chip_played)
+	    
+	    print("\n\n")
+
+	    return chip_stats_dict
