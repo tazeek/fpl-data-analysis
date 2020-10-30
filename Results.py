f1959d8bfbd05d1aef42fd92a1c5b5961d16cd02
tazeek <tazeek.rakib@gmail.com>
2020-10-30 21:17:38 +0800

    Filter columns added

--- Results.py
+++ Results.py
@@ -10,3 +10,7 @@ class Results:
 		r = requests.get(url)
 
 		self.results_matches_df =  pd.DataFrame(r.json())
+
+	def drop_columns(self, drop_columns):
+
+		self.results_matches_df = self.results_matches_df.drop(drop_columns, axis=1)
