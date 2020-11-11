f8c0f651788ebd5cf0cce389c98db46b450415d8
tazeek <tazeek.rakib@gmail.com>
2020-11-11 21:57:34 +0800

    Constructor modified

--- Player.py
+++ Player.py
@@ -1,4 +1,8 @@
+import pandas as pd
+
 class Player:
 
-	def __init__(self):
-		pass
+	def __init__(self, players_json, player_type):
+		
+		self.players_df = pd.DataFrame(players_json)
+		self.players_types_df = pd.DataFrame(player_type)
