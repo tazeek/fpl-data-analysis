caf9e3ee597da20049c220a315e6b2ef9e9f97d9
tazeek <tazeek.rakib@gmail.com>
2020-12-11 10:58:13 +0800

    Improved display graph

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from APIConnector import APIConnector
 from GameweekStats import GameweekStats
 from Teams import Teams
 
+import plotly.graph_objects as go
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -12,6 +14,10 @@ def initialize_app(app):
 	results_obj = Results()
 	api_connector = APIConnector()
 	gameweek_stats = GameweekStats(api_connector.get_events_gameweeks())
+	team_info = Teams(api_connector.get_teams_information())
+
+	team_df = team_info.return_dataframe_obj()
+	results_obj.map_teams(team_df)
 
 	future_opp_score_df = results_obj.get_future_opponents_stats(gameweek_stats.current_gameweek_number())
 
@@ -21,7 +27,8 @@ def initialize_app(app):
             orientation='h'))
 
 	fig.update_layout(
-		title="Average FDR for teams (Next 4 matches)"
+		title="Average FDR for teams (Next 4 matches)",
+		height = 700
 	)
 
 	fig.update_xaxes(
@@ -30,10 +37,9 @@ def initialize_app(app):
         title_standoff = 25
 	)
 
-	fig.update_yaxes(
-		title_text = "Teams",
-	   title_standoff = 25
-	)
+	app.layout = html.Div([
+		dcc.Graph(id='fdr-display',figure=fig)
+	])
 
 if __name__ == '__main__':
 
