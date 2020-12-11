717575bf18d3c0b6bf75decf62cc24a378eee871
tazeek <tazeek.rakib@gmail.com>
2020-12-11 10:46:56 +0800

    Loaded objects

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from Results import Results
+from APIConnector import APIConnector
+from GameweekStats import GameweekStats
+from Teams import Teams
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+
+results_obj = Results()
+api_connector = APIConnector()
+gameweek_stats = GameweekStats(api_connector.get_events_gameweeks())
+
+future_opp_score_df = results_obj.get_future_opponents_stats(gameweek_stats.current_gameweek_number())
