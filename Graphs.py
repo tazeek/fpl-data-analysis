d306bbbc4ff94a8c0afa31d7f3321abcbadd43d6
tazeek <tazeek.rakib@gmail.com>
2020-12-13 01:17:05 +0800

    FDR code moved to Graphs

--- Graphs.py
+++ Graphs.py
@@ -12,3 +12,33 @@ class Graphs:
 		
 		self._gameweek_stats = GameweekStats(self._api_connector.get_events_gameweeks())
 		self._team_info = Teams(self._api_connector.get_teams_information())
+
+	def get_future_fdr_scores(self):
+
+		results_obj = self._results_obj
+		team_df = self._team_info.return_dataframe_obj()
+
+		results_obj.map_teams(team_df)
+
+		future_opp_score_df = results_obj.get_future_opponents_stats(gameweek_stats.current_gameweek_number())
+
+		fig = go.Figure(
+			go.Bar(
+				x=future_opp_score_df['difficulty'],
+				y=future_opp_score_df['team'],
+				orientation='h'
+			)
+		)
+
+		fig.update_layout(
+			title="Average FDR for teams (Next 4 matches)",
+			height = 700
+		)
+
+		fig.update_xaxes(
+			title_text = "Average FDR",
+			tickangle = 45,
+			title_standoff = 25
+		)
+
+		return fig
