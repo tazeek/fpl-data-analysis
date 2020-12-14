36c9d8a62dda09421b6396ca420176335331040c
tazeek <tazeek.rakib@gmail.com>
2020-12-15 07:12:23 +0800

    Remove usage of variable

--- Graphs.py
+++ Graphs.py
@@ -11,12 +11,13 @@ class Graphs:
 
 		self._api_connector = APIConnector()
 		
-		self._gameweek_stats = GameweekStats(self._api_connector.get_events_gameweeks())
 		self._team_info = Teams(self._api_connector.get_teams_information())
 
 		self._results_obj = Results(self._team_info)
+		self._gameweek_results = self._results_obj.prepare_gameweek_stats()
 
-		self._gameweek_number = self._gameweek_stats.current_gameweek_number().item() # convert int64 to int
+		self._gameweek_obj = GameweekStats(self._api_connector.get_events_gameweeks())
+		self._gameweek_number = self._gameweek_obj.current_gameweek_number().item() # convert int64 to int
 		self._x_values = [i for i in range(1, self._gameweek_number + 1)]
 
 	def get_future_fdr_scores_fig(self):
@@ -46,7 +47,7 @@ class Graphs:
 
 	def get_chips_stats_fig(self):
 
-		chip_stats_df = self._gameweek_stats.fetch_chip_stats()
+		chip_stats_df = self._gameweek_obj.fetch_chip_stats()
 
 		chip_stats_df.rename(
 			columns = {
@@ -91,8 +92,6 @@ class Graphs:
 
 	def get_goals_scored_stats_fig(self):
 
-		gw_stats_df = self._results_obj.prepare_gameweek_stats()
-
 		fig = go.Figure() 
 
 		for column in ['home_team','away_team']:
@@ -100,7 +99,7 @@ class Graphs:
 			fig.add_trace(
 				go.Scatter(
 					x=self._x_values, 
-					y=gw_stats_df[column],
+					y=self._gameweek_results[column],
 					mode='lines+markers',
 					name=column
 				)
@@ -127,8 +126,6 @@ class Graphs:
 
 	def get_clean_sheets_stats_fig(self):
 
-		gw_stats_df = self._results_obj.prepare_gameweek_stats()
-
 		fig = go.Figure()
 
 		for column in ['home_team_cs','away_team_cs']:
@@ -136,7 +133,7 @@ class Graphs:
 			fig.add_trace(
 				go.Scatter(
 					x=self._x_values, 
-					y=gw_stats_df[column],
+					y=self._gameweek_results[column],
 					mode='lines+markers',
 					name=column
 				)
