cf96e0b9fb0b6015b8cea26c7b660c07c324ac25
tazeek <tazeek.rakib@gmail.com>
2020-12-30 07:27:40 +0800

    URL fetcher added

--- User.py
+++ User.py
@@ -1,7 +1,15 @@
-import pandas as pd:
+import pandas as pd
+import requests
 
 class User:
 
 	def __init__(self, id):
 
 		self.fpl_id = id
+
+	def fetch_current_squad(self):
+
+		url = f"https://fantasy.premierleague.com/api/my-team/{self.fpl_id}/"
+		r = requests.get(url)
+
+		return r
