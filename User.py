f9086d065f93b9f3e0120a797c30af610b6f6fc7
tazeek <tazeek.rakib@gmail.com>
2020-12-30 19:55:32 +0800

    Authentication steps done

--- User.py
+++ User.py
@@ -3,13 +3,35 @@ import requests
 
 class User:
 
-	def __init__(self, id):
+	def __init__(self, id, email, password):
 
-		self.fpl_id = id
+		self._fpl_id = id
+		self._email = email
+		self._password = password
+		self._session = None
+
+		self._login()
 
 	def fetch_current_squad(self):
 
-		url = f"https://fantasy.premierleague.com/api/my-team/{self.fpl_id}/"
-		r = requests.get(url)
+		url = f"https://fantasy.premierleague.com/api/my-team/{self._fpl_id}/"
+		r = self._session.get(url)
 
 		return r
+
+	def _login(self):
+
+		self._session = requests.session()
+
+		url = 'https://users.premierleague.com/accounts/login/'
+
+		payload = {
+		 	'password': self._password,
+			'login': self._email,
+			'redirect_uri': 'https://fantasy.premierleague.com/a/login',
+			'app': 'plfpl-web'
+		}
+
+		self._session.post(url, data=payload)
+
+		return None
