45e2d872b90d38c8a2bbc0519d809b588fabec28
tazeek <tazeek.rakib@gmail.com>
2021-01-01 23:15:44 +0800

    Code added for filtering

--- Player.py
+++ Player.py
@@ -2,7 +2,14 @@ import pandas as pd
 
 class Player:
 
-	def __init__(self, players_json, player_type):
+	def __init__(self, players_json):
 		
-		self.players_df = pd.DataFrame(players_json)
-		self.players_types_df = pd.DataFrame(player_type)
+		self._players_df = pd.DataFrame(players_json)
+
+	def _prepare_data(self):
+
+		df = self._players_df
+		df['name'] = player_info_df['first_name'] + ' ' + player_info_df['second_name']
+		df['now_cost'] = player_info_df['now_cost'] / 10
+
+		self._players_df = df[['name','team','id','element_type','selected_by_percent','now_cost','minutes','total_points']]
