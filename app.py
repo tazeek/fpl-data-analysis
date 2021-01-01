fc273ccaa331e2d66606f9f6433fed27f2f9ddf8
tazeek <tazeek.rakib@gmail.com>
2021-01-01 10:18:54 +0800

    Remove unused modules

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from layout import generate_layout
 
-import plotly.graph_objects as go
-
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
 
 app = dash.Dash()
 server = app.server
