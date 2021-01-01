dd300358096a12b47037aa60f224620fe3fdafee
tazeek <tazeek.rakib@gmail.com>
2021-01-01 10:18:13 +0800

    Layout file segmented

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from Graphs import Graphs
+from layout import generate_layout
 
 import plotly.graph_objects as go
 
@@ -6,19 +6,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-graphs_obj = Graphs()
-
 app = dash.Dash()
 server = app.server
 
-app.layout = html.Div([
-	dcc.Graph(id='fdr-display',figure=graphs_obj.get_future_fdr_scores_fig()),
-	dcc.Graph(id='show-all-chips',figure=graphs_obj.get_chips_stats_fig()),
-	dcc.Graph(id='fpl-scores-stats',figure=graphs_obj.get_fpl_scores_stats()),
-	dcc.Graph(id='fpl-transfers-stats',figure=graphs_obj.get_transfers_stats()),
-	dcc.Graph(id='goals-scored-fig',figure=graphs_obj.get_goals_scored_stats_fig()),
-	dcc.Graph(id='clean-sheet-stats',figure=graphs_obj.get_clean_sheets_stats_fig())
-])
+app.layout = generate_layout()
 
 if __name__ == '__main__':
 
