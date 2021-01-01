dfdaf3647ef0fedc886576bb9361bfb98784d005
tazeek <tazeek.rakib@gmail.com>
2021-01-01 23:56:10 +0800

    Recursion bug fixed

--- APIConnector.py
+++ APIConnector.py
@@ -1,6 +1,7 @@
 from GameweekStats import GameweekStats
 from Teams import Teams
 from Results import Results
+from Player import Player
 
 import requests
 
@@ -15,24 +16,28 @@ class APIConnector:
 
 		self._team_info = None
 		self._gameweek_obj = None
+		self._player_stats = None
 
 	def show_all_keys(self):
 		return self.json_object.keys()
 
 	def get_events_gameweeks(self):
 		if self._gameweek_obj is None:
-			self._gameweek_obj = GameweekStats(self._api_connector.get_events_gameweeks())
+			self._gameweek_obj = GameweekStats(self.json_object['events'])
 
 		return self._gameweek_obj
 
 	def get_teams_information(self):
 		if self._team_info is None:
-			self._team_info = Teams(self._api_connector.get_teams_information())
+			self._team_info = Teams(self.json_object['teams'])
 
 		return self._team_info
 
 	def get_player_information(self):
-		return self.json_object['elements']
+		if self._player_stats is None:
+			self._player_stats = Player(self.json_object['elements'])
+
+		return self._player_stats
 
 	def get_player_types(self):
 		return self.json_object['element_types']
